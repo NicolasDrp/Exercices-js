50f8971d07f113606ffe2e6b46bd82b0d29cc032
NicolasDrp <drapier.nicolas07@gmail.com>
2023-05-16 09:50:32 +0200

    ajout : bouton supprimer

--- To-Do List/script.js
+++ To-Do List/script.js
@@ -10,20 +10,64 @@ function ajouter(event) {
     let message = document.createElement("li");
 
     let check = document.createElement("INPUT");
+    check.className = "checkbox"
     check.setAttribute("type", "checkbox");
 
+
+    let supprimer = document.createElement("button");
+    supprimer.className = "supprimer"
+    supprimer.innerText = "Supprimer la tâche"
+
+    let modifier = document.createElement("button");
+    modifier.className = "modifier"
+    modifier.innerText = "Modifier"
+
     tache = document.getElementById("tache").value;
-  
-    
+
+
     document.getElementById("liste").appendChild(message);
     message.append(check);
     message.append(tache);
-    
+    message.append(supprimer);
+    message.append(modifier);
 
-    document.getElementById("tache").value = ""
+
+
+    document.getElementById("tache").value = "";
+
 
 }
 
-if (document.querySelector(input).checked == true){
-    alert("kjhcdiuhqeu")
-}
+// au clic d'une checkbox
+document.addEventListener("click", function (event) {
+    if (event.target.className === "checkbox") {
+        let listCheck = document.getElementsByClassName("checkbox");
+        for (let i = 0; i < listCheck.length; i++) {
+            let element = listCheck[i];
+            if (element.checked == true) {
+                element.parentElement.style.backgroundColor = "green";
+                element.parentElement.style.color = "white";
+
+
+            } else {
+                element.parentElement.style.color = "black";
+                element.parentElement.style.backgroundColor = "white";
+            }
+        }
+    }
+});
+
+
+//action bouton supprimer
+document.addEventListener("click", function (event) {
+        let listSuppr = document.getElementsByClassName("supprimer");
+        for (let i = 0; i < listSuppr.length; i++) {
+            let element = listSuppr[i];
+            element.addEventListener("click", function() {
+                element.parentElement.remove();
+             });
+        }
+});
+
+
+//à l'appuie du bouton supprimer
